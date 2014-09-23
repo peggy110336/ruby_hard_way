c9fdd3a44b0981204cd7a15cca733b796124fc4e
Peggy <K-Peggy@PeggydeMac.local>
2014-09-23 09:41:22 -0700

    Exercise1 and Study Drills

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+#puts "Hello World!"
+puts "Hello K-Peggy"
+#puts "I like typing Ruby."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd musch rather you 'not'."
+#puts 'I "said" do not touch this.'
+#puts "This line is Study Drills' practice."
