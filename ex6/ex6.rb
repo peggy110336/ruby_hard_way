d27158ff903e92fdb12c9bc209e45282c9abb96d
Peggy <K-Peggy@PeggydeMac.local>
2014-09-24 10:24:19 -0700

    add comment for each line to explain meaning

--- ex6/ex6.rb
+++ ex6/ex6.rb
@@ -1,21 +1,35 @@
+#set vale of "types_of_ people"
 types_of_people = 10
+#set String x and insert value types_of_people
 x = "There are #{types_of_people} types of people."
+#set String binsry
 binary = "binary"
+#set String do_not
 do_not = "don't"
+#set String y and insert String binary,do_not
 y = "Those who know #{binary} and those who #{do_not}."
 
+#puts x,y
 puts x
 puts y
 
+#puts String which insert String x
 puts "I said: #{x}."
+#puts String which insert String y
 puts "I also said: '#{y}'."
 
+#set hilarious value is false
 hilarious = false
+#set joke_evalution String and insert value hilarious
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+#puts String joke_evalition
 puts joke_evaluation
 
+#set String w
 w = "This is the left side of..."
+#set String e
 e = "a string with a right side."
 
+#puts the String which w combined of e
 puts w + e
