4ff6d474cb652fa6217c0bcb8d64b5559a8c9199
Peggy <K-Peggy@PeggydeMac.local>
2014-09-24 09:45:33 -0700

    Study Drills. delete [my_]

--- ex5/ex5.rb
+++ ex5/ex5.rb
@@ -1,18 +1,17 @@
-my_name = 'K-Peggy'
-my_age = 20
-my_height = 74
-my_weight = 180
-my_eyes = 'black'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'K-Peggy'
+age = 20
+height = 74
+weight = 180
+eyes = 'black'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about %s." % my_name
-puts "He's %d inches tall." % my_height
-puts "He's %d pounds heavy." % my_weight
+puts "Let's talk about %s." % name
+puts "He's %d inches tall." % height
+puts "He's %d pounds heavy." % weight
 puts "Actually that's not heavy."
-puts "He got %s eyes and %s hair." % [my_eyes,my_hair]
-puts "His teeth are usually %s depending on the coffee." % my_teeth
-
+puts "He got %s eyes and %s hair." % [eyes,hair]
+puts "His teeth are usually %s depending on the coffee." % teeth
 
 #This line is ticky, try to get it exactly right
-puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age+my_height+my_weight]
+puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age+height+weight]
