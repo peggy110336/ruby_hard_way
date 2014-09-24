cca4bba6eff363d37b393c5a9584cff3af683f57
Peggy <K-Peggy@PeggydeMac.local>
2014-09-23 21:42:48 -0700

    ann comment to each line

--- ex3/ex3.rb
+++ ex3/ex3.rb
@@ -1,23 +1,37 @@
+#輸出"I will now count my chickens:"
 puts "I will now count my chickens:"
 
+#輸出"Hens",and print the answer of #{25 + 30 /6}
 puts "Hens #{25 + 30 / 6}"
+#輸出"Hens",and print the answer of #{100 - 25 * 3 % 4}
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+#輸出"Now I will count the eggs:"
 puts "Now I will count the eggs:"
+#print the answer of this math question.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+#puts this sentence.
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+#puts true or false about this quetion. It will print false.
 puts 3 + 2 < 5 - 7
 
+#puts sentence then print the answer of 3+2.
 puts "What is 3 + 2? #{3 + 2}"
+#puts sentence then print the answer of 5-7.
 puts "What is 5 - 7? #{5 - 7}"
 
+#puts this sentence.
 puts "oh, thst's why it's false."
 
+#puts this sentence.
 puts "How about some more."
 
+#puts true or false about this question #{5 > -2}. It will print true.
 puts "Is it greater? #{5 > -2}"
+#puts true or false about this question #{5 >= -2}. It will print true.
 puts "Is it greater or equal? #{5 >= -2}"
+#puts true or false about this question #{5 <= -2}. Iy will print false.
 puts "Is it less or equal? #{5 <= -2}"
 
