324106dfd81f625b8465f818499422d8140bdd47
Peggy <K-Peggy@PeggydeMac.local>
2014-09-24 09:07:43 -0700

    Why NameError? Because variable not found.

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -4,7 +4,7 @@ drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
+#carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
