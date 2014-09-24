ec2024e1e05b577d27fb516147c0d68a6fae5307
Peggy <K-Peggy@PeggydeMac.local>
2014-09-24 09:14:52 -0700

    write comment for each variable assignment

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -1,10 +1,18 @@
+#set car is 100
 cars = 100
+#set space_in_a_car is 4.0 (float)
 space_in_a_car = 4.0
+#set there are 30 drivers.
 drivers = 30
+#set there are 90 passengers
 passengers = 90
+#set how many cars are not driven.
 cars_not_driven = cars - drivers
+#set how many cars are driven
 cars_driven = drivers
-#carpool_capacity = cars_driven * space_in_a_car
+#set the area of carpool,only for cars what are driven
+carpool_capacity = cars_driven * space_in_a_car
+#set how many passengers sit of per car
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
