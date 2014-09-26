2a39ba26cadf0296ec54fe715705c4550defaade
Peggy <K-Peggy@PeggydeMac.local>
2014-09-26 00:12:10 -0700

    other way to print an artical.

--- ex9/ex9.rb
+++ ex9/ex9.rb
@@ -4,9 +4,11 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
-puts """
+puts <<PARAGRAPH
+
 There's something going on here.
 With the tree double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-"""
+
+PARAGRAPH
