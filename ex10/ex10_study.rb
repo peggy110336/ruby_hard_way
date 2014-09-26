c2ca414c5bfb94a0b28ebfe48b1afb412c8716bd
Peggy <K-Peggy@PeggydeMac.local>
2014-09-26 07:21:49 -0700

    Exercise10 Study Drills

---
+++ ex10/ex10_study.rb
@@ -0,0 +1,16 @@
+name = "K-Peggy"
+school = "Providence University"
+major_subject = "Computer Science and Engineering."
+language = "%s %s %s" % ["Java", "PHP", "C"]
+
+Profile = <<My_information
+
+\tMy name is #{name}
+\tI'm studying at #{school}
+\tMy major_subject is #{major_subject}
+\tI can write #{language}
+\tI'm learing Ruby on Rails now
+
+My_information
+
+puts Profile
