d39320a9bea4eb093152a13479853874e8e4e1d9
Peggy <K-Peggy@PeggydeMac.local>
2014-09-25 23:08:44 -0700

    Exercise7 folder

---
+++ ex7/ex7.rb
@@ -0,0 +1,28 @@
+#puts it
+puts "Mary had a little lamb."
+#puts this String which has the String value #{'snow'}
+puts "It's fleece was white as #{'snow'}."
+#puts it
+puts "And everywhere that Mary went."
+#put 10 times "."
+puts "." * 10
+
+# set end1~end12 String value
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#what that print vs. puts on this line what's it do?
+#combined end1~end6 String,print it then it will not change line
+print end1 + end2 + end3 + end4 + end5 + end6
+#combined end7~end12 String, puts it then it will change line
+puts end7 + end8 + end9 + end10 + end11 + end12
