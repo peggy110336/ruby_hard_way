c46d1eb163fc5865e140e54c1db7b1230eb4ae28
Peggy <K-Peggy@PeggydeMac.local>
2014-09-27 00:39:16 -0700

    Exercise13 Study Drills

---
+++ ex13/ex13_study_fewer.rb
@@ -0,0 +1,5 @@
+mother, father = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your mother's name is #{mother}."
+puts "Your father's name is #{father}."
---
+++ ex13/ex13_study_more.rb
@@ -0,0 +1,4 @@
+language1, language2, language3, language4 = ARGV
+
+puts "The script is called: #{$0}"
+puts "I can write #{language1}, #{language2}, #{language3}, #{language4}."
