7cf243c93a5fc3f4eb43de998d3093540f189bea
Peggy <K-Peggy@PeggydeMac.local>
2014-09-27 00:04:52 -0700

    Exercise11 Example.

---
+++ ex11/ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets
+print "How tall are you? "
+height = gets
+print "How much do you weight? "
+weight = gets
+
+puts "So,you're #{age} old, #{height} tall and #{weight} heavy."
