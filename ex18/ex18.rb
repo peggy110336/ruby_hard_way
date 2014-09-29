63670cb543764ebe5f103968dc5083b2b3b3762e
Peggy <K-Peggy@PeggydeMac.local>
2014-09-29 10:09:04 -0700

    Compelete Exercise18

---
+++ ex18/ex18.rb
@@ -0,0 +1,25 @@
+#This one is lihe your scripts with argv
+def print_two(*args)
+	arg1, arg2 = args
+	puts "arg1: #{arg1}, args: #{arg2}"
+end
+
+#ok, that *args is actually pitless, we can just do it
+def print_two_again(arg1, arg2)
+	puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+# this just takes one argument
+def print_one(arg1)
+	puts "arg1: #{arg1}"
+end
+
+# this one takes no arguments
+def print_none()
+	puts "I got nothin'."
+end
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
